7c8deba37aa82425e93d5ec95a3e71f834b42c58
milescsmith <mileschristiansmith@gmail.com>
2018-12-16 17:01:40 -0600

    Renaming and reformatting into package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='Azimuth',
+      version='2.0',
+      author='Nicolo Fusi and Jennifer Listgarten',
+      author_email="fusi@microsoft.com, jennl@microsoft.com",
+      description=("Machine Learning-Based Predictive Modelling of CRISPR/Cas9 guide efficiency"),
+      packages=["azimuth", "azimuth.features", "azimuth.models", "azimuth.tests"],
+      package_dir={'azimuth': 'azimuth'},
+      package_data={'azimuth': ['saved_models/*.*','data/*.*']},
+      install_requires=['scipy', 'numpy', 'matplotlib', 'nose', 'scikit-learn', 'pandas', 'biopython','GPy','ipyparallel', 'mkl',
+                        'hyperopt', 'paramz', 'theanets', 'numba'],
+      license="BSD",
+      # ext_modules=cythonize("ssk_cython.pyx"),
+      )
